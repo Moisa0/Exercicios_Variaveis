3b6d9d182ff2c2e419264e81dd5b86dfdd1a4b75
Moisa0 <95927203+Moisa0@users.noreply.github.com>
2023-08-18 14:34:43 -0300

    coloquei uma limitação de 2 casas decimais

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,6 @@
 let pesoDaPessoa = 76;
 let alturaDaPessoa = 1.79;
 
-Imc = pesoDaPessoa/Math.pow(alturaDaPessoa, 2);
+Imc = (pesoDaPessoa/Math.pow(alturaDaPessoa, 2)).toFixed(2);
 
 console.log(Imc);
